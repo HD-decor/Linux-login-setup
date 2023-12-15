4ff89ab7693ff3dd2b560e0f571644bcf2c1f6a6
Kimi Syrjä <kimi.syrja@gmail.com>
2023-12-15 22:38:30 +0200

    Add username input

---
+++ setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Ask username from user
+read -p "Enter the username: " SERVER_USER
+
+# Define basic variables
+GIT_REPO="https://github.com/HD-decor/Linux-login-setup"
+TEMP_DIR=$(mktemp -d)
+
+# Check if the user exists
+if id "$SERVER_USER" &>/dev/null; then
+    echo "User $SERVER_USER already exists."
+else
+    sudo useradd -m -s /bin/bash "$SERVER_USER"
+    sudo mkdir -p /home/"$SERVER_USER"/.ssh
+    sudo chown -R "$SERVER_USER":"$SERVER_USER" /home/"$SERVER_USER"/.ssh
+    sudo chmod 700 /home/"$SERVER_USER"/.ssh
+    sudo usermod -aG sudo "$SERVER_USER"
+    echo "User $SERVER_USER created and added to sudo group."
+fi
+
+# Clone the Git repository containing SSH keys
+git clone "$GIT_REPO" "$TEMP_DIR"
+
+# Check if the directory exists and contains files
+if [ -d "$TEMP_DIR" ] && [ "$(ls -A "$TEMP_DIR")" ]; then
+    # Copy keys to the user's authorized_keys file
+    cat "$TEMP_DIR"/keys/*.pub | sudo tee -a /home/"$SERVER_USER"/.ssh/authorized_keys > /dev/null
+    sudo chown "$SERVER_USER":"$SERVER_USER" /home/"$SERVER_USER"/.ssh/authorized_keys
+    sudo chmod 600 /home/"$SERVER_USER"/.ssh/authorized_keys
+
+    echo "SSH keys added to $SERVER_USER's authorized_keys file."
+else
+    echo "Failed to fetch SSH keys from the repository."
+fi
+
+# Configure SSH to only allow RSA keys and disable root login
+sudo sed -i '/^PermitRootLogin/s/yes/no/' /etc/ssh/sshd_config
+sudo sed -i '/^PasswordAuthentication/s/yes/no/' /etc/ssh/sshd_config
+sudo sed -i '/^ChallengeResponseAuthentication/s/yes/no/' /etc/ssh/sshd_config
+sudo systemctl restart sshd.service
+
+echo "SSH configuration updated: Root login disabled, Password authentication disabled, RSA keys enforced."
+
+# Clean up temporary directory
+rm -rf "$TEMP_DIR"
