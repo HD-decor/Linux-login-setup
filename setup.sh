e40c8bb4869d2430f15af795ef5bd9425454fd74
Kimi Syrjä <kimi.syrja@gmail.com>
2023-12-16 02:10:51 +0200

    Fix authorized_keys missing

--- setup.sh
+++ setup.sh
@@ -1,95 +1,88 @@
 #!/bin/bash
 
-# Detect install software
+# Detect package manager
 if command -v yum &> /dev/null; then
     packagesystem="yum"
 elif command -v apt-get &> /dev/null; then
     packagesystem="apt"
 else
-    echo "This script don't support your system."
+    echo "This script doesn't support your system."
     exit 1
 fi
 
-# Update status
-read -p "Do you want run yum/apt update and install basic stuff before install git? (y/n): " update_status
+# Update and install basic tools
+read -p "Do you want to update and install basic tools before installing Git? (y/n): " update_status
 
-
-# Install git on server
-if [ "$packagesystem" == "yum" ]; then
-    if [ "$update_status" == "y" ] || [ "$update_status" == "yes" ]; then
-        yum update -y
-        yum install tmux nano htop iotop -y
+if [[ "$update_status" =~ ^[Yy](es)?$ ]]; then
+    if [ "$packagesystem" == "yum" ]; then
+        sudo yum update -y
+        sudo yum install -y tmux nano htop iotop
+    elif [ "$packagesystem" == "apt" ]; then
+        sudo apt-get update -y
+        sudo apt-get install -y tmux nano htop iotop
     fi
-    yum install -y git
-elif [ "$packagesystem" == "apt" ]; then
-    if [ "$update_status" == "y" ] || [ "$update_status" == "yes" ]; then
-        apt-get update -y
-        apt-get install tmux nano htop iotop -y
-    fi
-    apt-get install -y git
 fi
 
-# Check if git is installed
+# Install Git
+if [ "$packagesystem" == "yum" ]; then
+    sudo yum install -y git
+elif [ "$packagesystem" == "apt" ]; then
+    sudo apt-get install -y git
+fi
+
+# Check if Git is installed
 if ! command -v git &> /dev/null; then
     echo "Git is not installed. Please install Git manually."
     exit 1
 fi
 
-
-# Ask username from user
+# Ask for username
 read -p "Enter the username: " SERVER_USER
 
-# Check if the username is provided
+# Validate username
 if [ -z "$SERVER_USER" ]; then
     echo "Username cannot be empty. Exiting...."
     exit 1
 fi
 
-# Clean old key-file
-if [ -e /home/"$SERVER_USER"/.ssh/authorized_keys ]; then
-    cat /dev/null > /home/"$SERVER_USER"/.ssh/authorized_keys
-    sleep 2
+# Clear old authorized_keys file if it exists
+if [ -e "/home/$SERVER_USER/.ssh/authorized_keys" ]; then
+    sudo truncate -s 0 "/home/$SERVER_USER/.ssh/authorized_keys"
 fi
 
 # Define basic variables
 GIT_REPO="https://github.com/HD-decor/Linux-login-setup"
 TEMP_DIR=$(mktemp -d)
 
-# Check if the user exists
+# Check if the user exists and create if not
 if id "$SERVER_USER" &>/dev/null; then
     echo "User $SERVER_USER already exists."
 else
     sudo useradd -m -s /bin/bash "$SERVER_USER"
-    sudo mkdir -p /home/"$SERVER_USER"/.ssh
-    sudo chown -R "$SERVER_USER":"$SERVER_USER" /home/"$SERVER_USER"/.ssh
-    sudo chmod 700 /home/"$SERVER_USER"/.ssh
+    sudo mkdir -p "/home/$SERVER_USER/.ssh"
+    sudo chown -R "$SERVER_USER:$SERVER_USER" "/home/$SERVER_USER/.ssh"
+    sudo chmod 700 "/home/$SERVER_USER/.ssh"
 fi
 
 # Clone the Git repository containing SSH keys
-git clone "$GIT_REPO" "$TEMP_DIR"
-
-# Check if the directory exists and contains files
-if [ -d "$TEMP_DIR" ] && [ "$(ls -A "$TEMP_DIR")" ]; then
-
-    echo "--- Keys ---"
-    cat "$TEMP_DIR"/keys/*.pub
-    echo "--- Keys end ---"
-
-    # Copy keys to the user's authorized_keys file
-    cat "$TEMP_DIR"/keys/*.pub | while read key; do
-        echo "$key" | sudo tee -a /home/"$SERVER_USER"/.ssh/authorized_keys > /dev/null
-        echo "" | sudo tee -a /home/"$SERVER_USER"/.ssh/authorized_keys > /dev/null
+if git clone "$GIT_REPO" "$TEMP_DIR"; then
+    # Append keys to the authorized_keys file
+    for keyfile in "$TEMP_DIR"/keys/*.pub; do
+        if [[ -f "$keyfile" ]]; then
+            sudo cat "$keyfile" | sudo tee -a "/home/$SERVER_USER/.ssh/authorized_keys" >/dev/null
+            sudo echo "" | sudo tee -a "/home/$SERVER_USER/.ssh/authorized_keys" >/dev/null
+        fi
     done
 
-    sudo chown "$SERVER_USER":"$SERVER_USER" /home/"$SERVER_USER"/.ssh/authorized_keys
-    sudo chmod 600 /home/"$SERVER_USER"/.ssh/authorized_keys
+    sudo chown "$SERVER_USER:$SERVER_USER" "/home/$SERVER_USER/.ssh/authorized_keys"
+    sudo chmod 600 "/home/$SERVER_USER/.ssh/authorized_keys"
 
     echo "SSH keys added to $SERVER_USER's authorized_keys file."
 else
     echo "Failed to fetch SSH keys from the repository."
 fi
 
-# Configure SSH to only allow RSA keys and disable root login
+# Configure SSH
 sudo sed -i '/^PermitRootLogin/s/yes/no/' /etc/ssh/sshd_config
 sudo sed -i '/^PasswordAuthentication/s/yes/no/' /etc/ssh/sshd_config
 sudo sed -i '/^ChallengeResponseAuthentication/s/yes/no/' /etc/ssh/sshd_config
@@ -97,5 +90,5 @@ sudo systemctl restart sshd.service
 
 echo "SSH configuration updated: Root login disabled, Password authentication disabled, RSA keys enforced."
 
-# Clean up temporary directory
+# Clean up temp directory
 rm -rf "$TEMP_DIR"
